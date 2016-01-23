60efdea0a431b80d74735f61e111397108b08780
Makemek <makemek101@hotmail.com>
2016-01-23 16:27:38 +0700

    add unit test file

---
+++ test/testDbConnectionModule.js
@@ -0,0 +1,14 @@
+var module = require('../lib/connection');
+var expect = require('chai').expect
+
+describe('Database connection module', function() {
+	describe('No configuration file', function() {
+		it("converts the basic colors", function() {
+			
+		});
+	});
+
+	describe('Invalid configuration file', function() {
+
+	});
+});
