c3633a94870c78e69d5bbec5d9958ac5701facf1
Makemek <makemek101@hotmail.com>
2016-01-23 12:43:21 +0700

    commit database configure template file

---
+++ db_config/config.js
@@ -0,0 +1,16 @@
+/*
+This is a database configuration file
+*/
+
+var host = '';
+var port = '';
+var database = '';
+
+var user = '';
+var password = '';
+
+exports.host = host;
+exports.port = port;
+exports.database = database;
+exports.user = user;
+exports.password = password;
