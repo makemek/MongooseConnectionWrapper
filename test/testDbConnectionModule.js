bf0817bfae0250ec339f3e23ec09375679a46832
Makemek <makemek101@hotmail.com>
2016-01-23 16:45:40 +0700

    add test case

--- test/testDbConnectionModule.js
+++ test/testDbConnectionModule.js
@@ -1,14 +1,26 @@
 var module = require('../lib/connection');
 var expect = require('chai').expect
+var notImplemented = 'Not implemented';
 
 describe('Database connection module', function() {
-	describe('No configuration file', function() {
-		it("converts the basic colors", function() {
-			
+	
+	describe('Invalid config file', function() {
+		it('No config file', function() {
+			throw notImplemented;
 		});
+
+		it('Non-existant file', function() {
+			throw notImplemented;
+		}); 
 	});
 
-	describe('Invalid configuration file', function() {
+	describe('Valid config file', function() {
+		it('Host not defined', function() {
+			throw notImplemented;
+		});
 
+		it('Database not defined', function() {
+			throw notImplemented;
+		});
 	});
 });
