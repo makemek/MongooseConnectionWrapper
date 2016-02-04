15521ac821c531ce6739048396c8566071980a6b
Makemek <makemek101@hotmail.com>
2016-02-05 00:30:46 +0700

    Export MongoConnection class

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 var Connection = require('./lib/connection.js');
 
-var connect2database = function(configJs) {
-	var db = new Connection(configJs);
-	return db.connect();
-}
-
-module.exports = connect2database;
+module.exports = Connection;
